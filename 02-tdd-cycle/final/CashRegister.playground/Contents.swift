063671abb140257777a1e4897acc2d63f2537054
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2023-03-15 06:27:09 +0300

    test with available funds sets available funds

---
+++ 02-tdd-cycle/final/CashRegister.playground/Contents.swift
@@ -0,0 +1,31 @@
+import Foundation
+import XCTest
+
+public class CashRegister {
+    
+    var availableFunds: Decimal = 0
+    
+    public init(availableFunds: Decimal = 0) {
+        self.availableFunds = availableFunds
+    }
+}
+
+class CashRegisterTests: XCTestCase {
+    
+    func test_init_createsCashRegister() {
+        XCTAssertNotNil(CashRegister())
+    }
+    
+    func test_init_withAvailableFunds_setsAvailableFunds() {
+        // given a certain condition
+        let availableFunds = Decimal(10)
+        
+        // when a certain action happens
+        let sut = CashRegister(availableFunds: availableFunds)
+        
+        // then an expected result occurs
+        XCTAssertEqual(sut.availableFunds,availableFunds)
+    }
+}
+
+CashRegisterTests.defaultTestSuite.run()
