60e97f14df7c050740efdbe7f2d4b2a4086399c0
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2023-03-15 06:39:41 +0300

    test add item adds cost to transaction total

--- 02-tdd-cycle/final/CashRegister.playground/Contents.swift
+++ 02-tdd-cycle/final/CashRegister.playground/Contents.swift
@@ -5,8 +5,14 @@ public class CashRegister {
     
     var availableFunds: Decimal = 0
     
-    public init(availableFunds: Decimal = 0) {
+    var transactionTotal: Decimal = 0
+    
+    public init(availableFunds: Decimal) {
         self.availableFunds = availableFunds
+    }
+    
+    public func addItem(_ cost: Decimal) {
+        transactionTotal = cost
     }
 }
 
@@ -21,6 +27,16 @@ class CashRegisterTests: XCTestCase {
         
         // then an expected result occurs
         XCTAssertEqual(sut.availableFunds,availableFunds)
+    }
+    
+    func test_addItem_oneItem_addsCostToTransactionTotal() {
+        let availableFunds = Decimal(10)
+        let sut = CashRegister(availableFunds: availableFunds)
+        let itemCost = Decimal(42)
+        
+        sut.addItem(itemCost)
+        
+        XCTAssertEqual(sut.transactionTotal, itemCost)
     }
 }
 
